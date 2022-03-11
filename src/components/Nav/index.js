118ed7d60d46cf9b13f0341f0267e4b0b4171a2c
Karan Sodhi <karanpreetsodhi1997@gmail.com>
2022-03-10 22:17:32 -0500

    header component created

---
+++ src/components/Nav/index.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function DisplayNavigation(){
+//about me, portfolio, contact, resume
+    return (
+        <div >
+            <ul>
+                <li>About Me</li>
+                <li>Portfolio</li>
+                <li>Contact</li>
+                <li>Resume</li>
+            </ul>
+        </div>
+
+    )
+}
+
+export default DisplayNavigation
---
+++ src/components/header/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import DisplayNavigation from "../../components/Nav"
+function DisplayHeader(){
+    return(
+
+        <div className="container na">
+            <h1>Karan Sodhi</h1>
+            <DisplayNavigation/>
+        </div>
+
+    )
+}
+
+export default DisplayHeader

cdb236651ac177ea631101b8875b999844d88756
Karan Sodhi <karanpreetsodhi1997@gmail.com>
2022-03-13 15:48:49 -0400

    footer icons completed

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,9 +1,15 @@
 import React from "react";
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { SocialIcon } from 'react-social-icons';
 function DisplayFooter(){
     return (
-        <div className="p-1 bg-dark text-center">
-            <h2>This is Footers</h2>
+        <div className="p-1 bg-dark text-center d-flex justify-content-center">
+            
+            <div className="m-2"><SocialIcon url="https://twitter.com/karansodhi"/></div>
+            <div className="m-2"><SocialIcon url="https://facebook.com/karan.sodhi" /></div>
+            <div className="m-2"><SocialIcon url="https://google.com/karan.sodhi" /></div>
+            <div className="m-2"><SocialIcon url="https://github.com/jaketrent" /></div>
+            
         </div>
     )
 }
